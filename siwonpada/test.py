71f866e7acc6d92895f22341411e8dc8ee952057
siwonpada <psw041115@gmail.com>
2023-02-26 15:42:05 +0900

    modify test.py in siwonpada

---
+++ siwonpada/test.py
@@ -0,0 +1,9 @@
+import sys
+import torch
+
+print(sys.version)
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name())
+print(torch.cuda.device_count())
